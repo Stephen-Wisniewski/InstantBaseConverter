a07f6f2b8fcb6b5eeb8e13a2a5626e321e1b6c20
Stephen_Wisniewski <stephen.p.wisniewski@gmail.com>
2016-12-11 17:21:15 +0000

    Added complete functionality for converting from a base 2 number to a base 10 number and vice versa. Added toasts (popup messages) to let the user know if the input is invalid.

--- app/src/main/java/omanix/instantbaseconverter/MainActivity.java
+++ app/src/main/java/omanix/instantbaseconverter/MainActivity.java
@@ -1,5 +1,6 @@
 package omanix.instantbaseconverter;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void convert(View view) {
@@ -30,8 +30,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else if(spinnerinput.equals("Base 2") && spinnerinput2.equals("Base 10")) {
-            output.setText(binaryToDecimal(input.getText().toString()));
+            String regex = "[0-1]+";
+
+            if (input.getText().toString().matches(regex)) {
+                output.setText(binaryToDecimal(input.getText().toString()));
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Invalid characters for a binary number!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
         }
+
+        else if(spinnerinput.equals("Base 10") && spinnerinput2.equals("Base 2")) {
+            String regex = "[0-9]+";
+
+            if(input.getText().toString().matches(regex)) {
+                output.setText(decimalToBinary(input.getText().toString()));
+            } else {
+                Context context = getApplicationContext();
+                CharSequence text = "Invalid characters for a decimal number!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        }
+
+
 
 
 
@@ -45,6 +73,13 @@ public class MainActivity extends AppCompatActivity {
     public static String binaryToDecimal(String input) {
         int decimalValue = Integer.parseInt(input, 2);
         return Integer.toString(decimalValue);
+    }
+
+
+
+    public static String decimalToBinary(String input) {
+        int decimalValue = Integer.parseInt(input);
+        return Integer.toBinaryString(decimalValue);
     }
 
 
