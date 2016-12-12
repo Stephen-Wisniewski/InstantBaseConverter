a6e43e25dcc52a5c9f78b10cde1492caac36ed13
Stephen_Wisniewski <stephen.p.wisniewski@gmail.com>
2016-12-12 00:53:46 +0000

    Cleaned up the code for showing the toast if invalid data is entered. Created a method for showing the toast instead of dumping all the code in one place.
    Added decimal to hex conversion and decimal to hex conversion

--- app/src/main/java/omanix/instantbaseconverter/MainActivity.java
+++ app/src/main/java/omanix/instantbaseconverter/MainActivity.java
@@ -1,7 +1,6 @@
 package omanix.instantbaseconverter;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner spinner2 = (Spinner) findViewById(R.id.spinner);
         Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+
         EditText input = (EditText) findViewById(R.id.input);
         EditText output = (EditText) findViewById(R.id.editText);
 
@@ -36,11 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 output.setText(binaryToDecimal(input.getText().toString()));
             } else {
                 Context context = getApplicationContext();
-                CharSequence text = "Invalid characters for a binary number!";
-                int duration = Toast.LENGTH_SHORT;
+                showToast(context, "Invalid characters for a base 2 number!");
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
             }
         }
 
@@ -51,35 +48,64 @@ public class MainActivity extends AppCompatActivity {
                 output.setText(decimalToBinary(input.getText().toString()));
             } else {
                 Context context = getApplicationContext();
-                CharSequence text = "Invalid characters for a decimal number!";
-                int duration = Toast.LENGTH_SHORT;
+                showToast(context, "Invalid characters for a base 10 number!");
+            }
+        }
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+        else if(spinnerinput.equals("Base 10") && spinnerinput2.equals("Base 16")) {
+            String regex = "[0-9]+";
+
+            if(input.getText().toString().matches(regex)) {
+                output.setText(decimalToHex(input.getText().toString()));
+            } else {
+                Context context = getApplicationContext();
+                showToast(context, "Invalid characters for a base 10 number!");
+            }
+        }
+
+        else if(spinnerinput.equals("Base 10") && spinnerinput2.equals("Base 8")) {
+            String regex = "[0-9]+";
+
+            if(input.getText().toString().matches(regex)) {
+                output.setText(decimalToOctal(input.getText().toString()));
+            } else {
+                Context context = getApplicationContext();
+                showToast(context, "Invalid characters for a base 10 number!");
             }
         }
 
 
-
-
-
-
-
-
     }
-
-
 
     public static String binaryToDecimal(String input) {
         int decimalValue = Integer.parseInt(input, 2);
         return Integer.toString(decimalValue);
     }
 
+    public static String binaryToHex(String input) {
+        return "x";
 
+    }
 
     public static String decimalToBinary(String input) {
         int decimalValue = Integer.parseInt(input);
         return Integer.toBinaryString(decimalValue);
+    }
+
+    public static String decimalToHex(String input) {
+        int decimalValue = Integer.parseInt(input);
+        return Integer.toHexString(decimalValue).toUpperCase();
+    }
+
+    public static String decimalToOctal(String input) {
+        int decimalValue = Integer.parseInt(input);
+        return Integer.toOctalString(decimalValue);
+    }
+
+
+
+    public static void showToast(Context context, String text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
 
 
